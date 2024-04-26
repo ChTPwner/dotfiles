cb7b2196ef3ded88e49491cd8253702284d72319
Thibault Delattre <thibault.delattre@kalido-pro.com>
2024-04-26 12:27:19 +0200

    neovim sessions

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -8,3 +8,5 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+vim.keymap.set("n", "<leader>or", "<cmd>Telescope neovim-project discover<cr>")
+vim.keymap.set("n", "<leader>oh", "<cmd>Telescope neovim-project history<cr>")
