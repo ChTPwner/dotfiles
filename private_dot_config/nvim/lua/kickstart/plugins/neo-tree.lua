d0ceb75ed1f721a3ae15b86921de8118a2e733fb
Thibault Delattre <thibothekoala@yahoo.fr>
2024-06-12 20:14:47 +0200

    fix neotree

--- private_dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ private_dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -8,7 +8,6 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
-    '3rd/image.nvim', -- optional image support
   },
   cmd = 'Neotree',
   keys = {
@@ -16,6 +15,9 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
