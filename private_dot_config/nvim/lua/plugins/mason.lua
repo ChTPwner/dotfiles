4603641a23a9ef96d3d613ebd7dfe8b3f769e89e
Thibault Delattre <thibault.delattre@eurotunnel.com>
2024-11-06 12:40:33 +0100

    push lazyvim conf

---
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+      "terraform-ls",
+      "tflint",
+      "tfsec",
+    },
+  },
+}
