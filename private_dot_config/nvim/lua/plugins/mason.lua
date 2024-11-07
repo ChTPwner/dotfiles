0517b3b029c594fdf9408bd1147283c064213e06
Thibault Delattre <thibault.delattre@eurotunnel.com>
2024-11-07 11:04:24 +0100

    nvim - update treesitter and mason config

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -3,10 +3,16 @@ return {
   opts = {
     ensure_installed = {
       "stylua",
+      "actionlist",
       "shfmt",
       "terraform-ls",
       "tflint",
       "tfsec",
+      "black",
+      "isort",
+      "clangd",
+      "dockerfile-language-server",
+      "helm-ls",
     },
   },
 }
